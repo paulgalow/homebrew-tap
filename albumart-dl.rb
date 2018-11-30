11bca62b0d2fcec0f3f1effc27b3f528a2a8e953
Paul Galow <paul.galow@gmail.com>
2018-11-30 22:35:54 +0100

    Update dependencies

--- albumart-dl.rb
+++ albumart-dl.rb
@@ -4,48 +4,11 @@ class AlbumartDl < Formula
   desc "Download HQ album cover art"
   homepage "https://github.com/paulgalow/albumart-dl"
   url "https://github.com/paulgalow/albumart-dl/releases/download/v0.1.0/albumart-dl-0.1.0.tar.gz"
-  sha256 "624493c47fd403b44447ad8dd7ce3cbf9ff97630099fa9cd4f3a8f655501255e"
+  sha256 "1f40f57ee4cd24dcebe60ab50585301c1bc9356885223112e5fe4660f035d54f"
 
   bottle :unneeded
 
   depends_on "python"
-
-  # Halo dependencies
-
-  resource "colorama" do
-    url "https://files.pythonhosted.org/packages/e6/76/257b53926889e2835355d74fec73d82662100135293e17d382e2b74d1669/colorama-0.3.9.tar.gz"
-    sha256 "48eb22f4f8461b1df5734a074b57042430fb06e1d61bd1e11b078c0fe6d7a1f1"
-  end
-
-  resource "cursor" do
-    url "https://files.pythonhosted.org/packages/23/0d/bec47260080d5e4b1d88a08b31c6c44b476a9c7509e41b1d84967027d32d/cursor-1.2.0.tar.gz"
-    sha256 "8ee9fe5b925e1001f6ae6c017e93682583d2b4d1ef7130a26cfcdf1651c0032c"
-  end
-
-  resource "enum34" do
-    url "https://files.pythonhosted.org/packages/bf/3e/31d502c25302814a7c2f1d3959d2a3b3f78e509002ba91aea64993936876/enum34-1.1.6.tar.gz"
-    sha256 "8ad8c4783bf61ded74527bffb48ed9b54166685e4230386a9ed9b1279e2df5b1"
-  end
-
-  resource "log_symbols" do
-    url "https://files.pythonhosted.org/packages/9a/bf/e9509fe2481a4e2b0432dad049126320d5aac5b20edbe67c2e993a151178/log_symbols-0.0.11.tar.gz"
-    sha256 "87be2f283cd6f455d89b76abcf2805fad5692ec9dcd8a31d10a4c975f51392eb"
-  end
-
-  resource "six" do
-    url "https://files.pythonhosted.org/packages/16/d8/bc6316cf98419719bd59c91742194c111b6f2e85abac88e496adefaf7afe/six-1.11.0.tar.gz"
-    sha256 "70e8a77beed4562e7f14fe23a786b54f6296e34344c23bc42f07b15018ff98e9"
-  end
-
-  resource "spinners" do
-    url "https://files.pythonhosted.org/packages/01/3f/156b7faa2cbadd35a504c9ee852db32e07ff5f106bbf288d49b22b061135/spinners-0.0.23.tar.gz"
-    sha256 "f396fea1ee00c0622988d7d2bf2895d26dd6f70850ca2ce94eaca52ca4873560"
-  end
-
-  resource "termcolor" do
-    url "https://files.pythonhosted.org/packages/8a/48/a76be51647d0eb9f10e2a4511bf3ffb8cc1e6b14e9e4fab46173aa79f981/termcolor-1.1.0.tar.gz"
-    sha256 "1d6d69ce66211143803fbc56652b41d73b4a400a2891d7bf7a1cdf4c02de613b"
-  end
 
   # Requests dependencies
 
@@ -71,9 +34,9 @@ class AlbumartDl < Formula
 
   # Direct dependencies
 
-  resource "halo" do
-    url "https://files.pythonhosted.org/packages/3a/16/1bd5c1364a898c58be8ab2c399654340d70825db5758bcf1bd42f2613aae/halo-0.0.22.tar.gz"
-    sha256 "35c42d33ee6a62e0d3bca5dc9a1bc1e1c22464190d8fca4f1205934c80836470"
+  resource "yaspin" do
+    url "https://files.pythonhosted.org/packages/fd/a2/2fb522b0d54b278e6ab52a2ffc4d3347668440c06306bb9308667701f4cb/yaspin-0.14.0.tar.gz"
+    sha256 "8e52bf8079a48e2a53f3dfeec9e04addb900c101d1591c85df69cf677d3237e7"
   end
 
   resource "requests" do
